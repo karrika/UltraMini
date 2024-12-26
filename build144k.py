0151804ef4a588a542bd51cfbea0512881cb7f6d
karri <karri@sipo.fi>
2024-12-26 10:15:17 +0200

    New files

---
+++ build144k.py
@@ -0,0 +1,26 @@
+import sys
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 build144k.py <filename>")
+        return
+
+    filename = sys.argv[1]
+
+    try:
+        with open(filename, 'rb') as f:
+            data = f.read()
+        if len(data) != (128 + 16) * 1024 + 128:
+            print("Size should be", (128 + 16) * 1024 + 128, "not", len(data))
+            quit()
+        filename = filename.rsplit( ".", 1)[0] + ".bin"
+        print(filename)
+        g = open(filename, 'wb')
+        g.write(data[128 + 16 * 1024:])
+        g.write(data[128:128 + 16 * 1024])
+    except FileNotFoundError:
+        print(f"Error: The file '{filename}' was not found.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    main()
