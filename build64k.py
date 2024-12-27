a75c9f527e25e22bd97748d4dcf47d36441a8a8b
karri <karri@sipo.fi>
2024-12-27 18:12:20 +0200

    Update

---
+++ build64k.py
@@ -0,0 +1,25 @@
+import sys
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 build64k.py <filename>")
+        return
+
+    filename = sys.argv[1]
+
+    try:
+        with open(filename, 'rb') as f:
+            data = f.read()
+        if len(data) != 64 * 1024 + 128:
+            print("Size should be", 64 * 1024 + 128, "not", len(data))
+            quit()
+        filename = filename.rsplit( ".", 1)[0] + ".bin"
+        g = open(filename, 'wb')
+        g.write(data[128:])
+        g.write(data[128:])
+    except FileNotFoundError:
+        print(f"Error: The file '{filename}' was not found.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    main()
