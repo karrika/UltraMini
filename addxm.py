3b46445f3c2eaf5d5168a8f7c889ff5e802a8b99
karri <karri@sipo.fi>
2025-01-04 11:03:24 +0200

    Add emberhdr.py

--- addxm.py
+++ addxm.py
@@ -1,8 +1,8 @@
 import sys
 ''' Add activation for 16k RAM and YM2152 sound chip in binary '''
 def main():
-    if len(sys.argv) != 2:
-        print("Usage: python3 addxm.py <filename>")
+    if len(sys.argv) < 2:
+        print("Usage: python3 addxm.py <filename> [xmpos] [bin]")
         return
 
     filename = sys.argv[1]
@@ -17,7 +17,12 @@ def main():
         FF7A    8d 70 04
         FF7D    4c 4a fe
         '''
-        index = len(data) - 128 - 8
+        if len(sys.argv) > 2:
+            xmpos = sys.argv[2]
+            index = len(data) - (int('10000',16) - int(xmpos,16))
+        else:
+            xmpos = 'ff78'
+            index = len(data) - 128 - 8
         data[index] = 0xa9
         index += 1
         data[index] = 0xe4
@@ -34,12 +39,15 @@ def main():
         index += 1
         data[index] = data[len(data) - 3]
         index = len(data) - 4
-        data[index] = 0x78
+        data[index] = int(xmpos[2:], 16)
         index += 1
-        data[index] = 0xff
-        filename = filename.rsplit( ".", 1)[0] + "p.a78"
+        data[index] = int(xmpos[0:2], 16)
+        filename = filename.rsplit( ".", 1)[0] + "xm.a78"
         g = open(filename, 'wb')
-        g.write(data)
+        if len(sys.argv) > 3:
+            g.write(data[128:])
+        else:
+            g.write(data)
     except FileNotFoundError:
         print(f"Error: The file '{filename}' was not found.")
     except Exception as e:
