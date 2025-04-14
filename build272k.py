ea487cc6a99c9d5b4bf70e24762b07f255d094cb
karri <karri@sipo.fi>
2025-04-14 11:02:07 +0300

    Add support for 272k files

---
+++ build272k.py
@@ -0,0 +1,27 @@
+import sys
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 build272k.py <filename>")
+        return
+
+    filename = sys.argv[1]
+
+    try:
+        with open(filename, 'rb') as f:
+            data = f.read()
+        if len(data) != (256 + 16) * 1024 + 128:
+            print("Size should be", (256 + 16) * 1024 + 128, "not", len(data))
+            quit()
+        filename = filename.rsplit( ".", 1)[0] + ".bin"
+        print(filename)
+        g = open(filename, 'wb')
+        for i in range(16):
+            g.write(data[128:128 + 16 * 1024])
+        g.write(data[128 + 16 * 1024:])
+    except FileNotFoundError:
+        print(f"Error: The file '{filename}' was not found.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    main()
